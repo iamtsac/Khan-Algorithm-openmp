9bc923cd752fe5bf44904d4c92e7ca9e1ed5f366
iamtsac <iamtsac@gmail.com>
2020-04-20 23:20:45 +0300

    vasiki idea khan

---
+++ khanprototype.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(){
+
+    do{
+        bool flag=false;
+        int i=0;
+        a=checkEdges();
+        matrix[v][h]=0;
+        tail[i]=a;
+
+            for (int k=0; k<size; k++) {
+                for (int l=0; l<size; l++) {
+                    if (matrix[k][l]==1) {
+                        flag=false;
+                        printf("there are more edges");
+                        break;
+                    }
+                }
+            }
+            i++
+        }
+    }while(!flag)
+}
+
