6f7195268495e1fe33cfb6a33837c6c0b77340c5
funkyCEID <jasonpavlakis@gmail.com>
2020-04-21 00:22:42 +0300

    Successful parsing of data

--- serial.c
+++ serial.c
@@ -22,21 +22,44 @@ int main(void)
 
   while( ( fgets(data, 500, fp)) !=  NULL ) //until we reach EOF
   {
-
-    if( line_count == 0 ) //do this if 1st line
+    if( line_count == 0 ) //do this if 1st data line
     {
-      fscanf(fp, "%c %d %d", &temp, &array_size, &total_edges);
-      printf("Array size: %d\n",array_size);
+      fscanf(fp, "%d %d %d", &array_size, &array_size, &total_edges);
+      printf("Array size: %d",array_size);
     }
-    else //do this for all other lines
+    line_count++;
+    break;
+  }
+  int matrix[array_size][array_size];
+  for(int i=0; i<array_size; i++)
+  {
+    for(int j=0; j<array_size; j++)
+    {
+      matrix[i][j]=0;
+    }
+  }
+
+
+  while( ( fgets(data, 500, fp)) !=  NULL ) //until we reach EOF
+  {
+    if( line_count != 1 ) //do this if not 1st data line
     {
       fscanf(fp, "%d %d", &temp1, &temp2);
+      matrix[temp1 - 1][temp2 - 1] = 1;
       printf("Temp1: %d\tTemp2: %d\n",temp1,temp2);
     }
     line_count++;
   }
 
   printf("Successfull parsing of data! Line count: %d\n",line_count);
+
+  for(int i=0; i<array_size; i++)
+  {
+    for(int j=0; j<array_size; j++)
+    {
+      printf("Node %d -> Node %d : %d\n",i+1,j+1,matrix[i][j]);
+    }
+  }
 
   fclose(fp);
 return 0;
