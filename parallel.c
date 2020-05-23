88731a11bc191999306349e946004a12d5a50e91
iamtsac <iamtsac@gmail.com>
2020-05-23 19:42:13 +0300

    no parallel yet but clenaer code

--- parallel.c
+++ parallel.c
@@ -6,6 +6,7 @@
 
 int array_size, total_edges;
 
+//Creating the structs
 struct node_info{
 int id;
 int out_edges;
@@ -41,6 +42,7 @@ struct node_info * pop (struct queue *q){ // gyrnaei to prwto node_info kai to b
   return tmp->data;
 }
 
+//Push And Pop
 void push (struct queue *q,struct node_info *newData){ // vazei sto telos ths ouras to neo data
   q->size++;
   if(q->front==NULL){
@@ -59,90 +61,71 @@ void push (struct queue *q,struct node_info *newData){ // vazei sto telos ths ou
   }
 }
 
-
-struct queue Kahn_Algorithm(struct queue *L, struct queue *S, struct node_info nodes[array_size], bool matrix[array_size][array_size])
+struct queue Kahn_Algorithm(struct queue *L, struct queue *S, struct node_info nodes[array_size], bool *matrix)
 {
 
-   
-      while(S->size > 0){
-        int count = 0;
-        struct node_info *n = pop(S);
-        push(L,n);
-#pragma omp parallel
-{
-        #pragma omp for
-        for (int i=0; i<array_size; i++)
+    while(S->size > 0){
+      int count = 0;
+      struct node_info *n = pop(S);
+      push(L,n);
+      for (int i=0; i<array_size; i++)
+      {
+        if( matrix[ ( n->id - 1 ) *array_size + i] == 1 )
         {
-          if( matrix[n->id - 1][i] == 1 )
-          {
-            matrix[n->id - 1][i] = false;
-            nodes[i].in_edges--;
-            nodes[n->id - 1].out_edges--;
-            if(nodes[i].in_edges == 0)
-            push(S,&nodes[i]);
-          }
+          matrix[ ( n->id - 1 ) *array_size + i] = false;
+          nodes[i].in_edges--;
+          nodes[n->id - 1].out_edges--;
+          if(nodes[i].in_edges == 0)
+          push(S,&nodes[i]);
         }
-        count++;
       }
- } 
-      if(L->size < array_size)
-      {
-       perror("The graph contains one or more cycles!\n");
-       exit(EXIT_FAILURE);
-      }
-      else
-      {
-        printf("Successful!\nThe Topological sort is: ");
-        return *L;
-      }
-      
-  }
+      count++;
+    }
+
+    if(L->size < array_size)
+    {
+     perror("The graph contains one or more cycles!\n");
+     exit(EXIT_FAILURE);
+    }
+    else
+    {
+      printf("Successful!\nThe Topological sort is: ");
+      return *L;
+    }
+}
 
 
 
-
-
-
-//MMMMMMMM               MMMMMMMM               AAA               IIIIIIIIIINNNNNNNN        NNNNNNNN
-//M:::::::M             M:::::::M              A:::A              I::::::::IN:::::::N       N::::::N
-//M::::::::M           M::::::::M             A:::::A             I::::::::IN::::::::N      N::::::N
-//M:::::::::M         M:::::::::M            A:::::::A            II::::::IIN:::::::::N     N::::::N
-//M::::::::::M       M::::::::::M           A:::::::::A             I::::I  N::::::::::N    N::::::N
-//M:::::::::::M     M:::::::::::M          A:::::A:::::A            I::::I  N:::::::::::N   N::::::N
-//M:::::::M::::M   M::::M:::::::M         A:::::A A:::::A           I::::I  N:::::::N::::N  N::::::N
-//M::::::M M::::M M::::M M::::::M        A:::::A   A:::::A          I::::I  N::::::N N::::N N::::::N
-//M::::::M  M::::M::::M  M::::::M       A:::::A     A:::::A         I::::I  N::::::N  N::::N:::::::N
-//M::::::M   M:::::::M   M::::::M      A:::::AAAAAAAAA:::::A        I::::I  N::::::N   N:::::::::::N
-//M::::::M    M:::::M    M::::::M     A:::::::::::::::::::::A       I::::I  N::::::N    N::::::::::N
-//M::::::M     MMMMM     M::::::M    A:::::AAAAAAAAAAAAA:::::A      I::::I  N::::::N     N:::::::::N
-//M::::::M               M::::::M   A:::::A             A:::::A   II::::::IIN::::::N      N::::::::N
-//M::::::M               M::::::M  A:::::A               A:::::A  I::::::::IN::::::N       N:::::::N
-//M::::::M               M::::::M A:::::A                 A:::::A I::::::::IN::::::N        N::::::N
-//MMMMMMMM               MMMMMMMMAAAAAAA                   AAAAAAAIIIIIIIIIINNNNNNNN         NNNNNNN 
+double gettime(void) 
+{
+   struct timeval tv;
+   gettimeofday(&tv, NULL);
+   return tv.tv_sec + 1e-6 * tv.tv_usec;
+}
 
 int main(void)
 {
   int temp1, temp2;
   char data[500];
-
-
+  double t1,t2;
+  struct node_info  *nodeItem;
+  struct queue q, S, L; 
   FILE *fp = fopen("dag.txt", "r");
+
   if(fp == NULL)
   {
     perror("Error while opening the file!\n");
     exit(EXIT_FAILURE);
   }
 
-  while( (fgets(data, 500, fp))) //read until we reach end of line
- {
-     fscanf(fp, "%d %d %d", &array_size, &array_size, &total_edges);
-     printf("Array size: %d x %d with %d edges\n",array_size,array_size,total_edges);
-     break;
- }
+  fscanf(fp, "%d %d %d", &array_size, &array_size, &total_edges);
+  printf("Array size: %d x %d with %d edges\n",array_size,array_size,total_edges);
+ 
+  struct node_info nodes[array_size];
 
-  struct node_info nodes[array_size]; // array of nodes
-  struct queue q;
   init(&q); //create of queue
+
+  t1=gettime();
 
   for(int i=0; i<array_size; i++)
   {
@@ -151,63 +134,58 @@ int main(void)
     nodes[i].out_edges = 0;
   }
 
-  bool matrix[array_size][array_size]; // array of edges between nodes
-  for(int i=0; i<array_size; i++)
-  {
-    for(int j=0; j<array_size; j++)
-    {
-      matrix[i][j]=0; // initializing all values of the array to be 0 (no relation/edges between nodes)
-    }
-  }
+  bool *matrix = (bool *)malloc(array_size *array_size * sizeof(bool ));
+
 
 
   while( (fgets(data, 500, fp)) !=  NULL ) // until we reach EOF
   {
     fscanf(fp, "%d %d", &temp1, &temp2);
-    if(feof(fp))
-    break;
-    matrix[temp1 - 1][temp2 - 1] = true; // initializing edges between nodes
+    if(feof(fp)) break;
+
+    matrix[(temp1 - 1 )* array_size + ( temp2 - 1 )] = 1;
     nodes[temp1 - 1].out_edges++; // setting out-edges of the node
     nodes[temp2 - 1].in_edges++; // setting in-edges of the node
 
   }
 
-  for(int i=0; i<array_size; i++) // printing matrix data
-  {
-    for(int j=0; j<array_size; j++)
-    {
-      printf("Node %d -> Node %d : %d\n",i+1,j+1,matrix[i][j]);
-    }
-    printf("\n\n\n");
-  }
-
-  printf("\n\n\n");
-  for(int i=0; i<array_size; i++) // printing node data
-  {
-    printf("id: %d\tout_edges: %d\tin_edges: %d\n",nodes[i].id, nodes[i].out_edges, nodes[i].in_edges);
-  }
-
-  struct queue S, L;
 
   init(&S);
 
   for(int i=0; i< array_size; i++)
   {
+
     if(nodes[i].in_edges == 0)
     push(&S, &nodes[i]);
+
   }
 
   init(&L);
 
   L = Kahn_Algorithm(&L, &S, nodes, matrix);
 
-  struct node_info  *nodeItem;
-  for(int i=0; i<array_size; i++){
+  t2=gettime();
+
+
+  for(int i=0; i<array_size; i++)
+  {
+
   nodeItem = pop(&L);
   printf("%d ",nodeItem->id);
-}
-  printf("\n");
+
+  }
+
+  printf("\n"); 
+  printf("time %lf  \n",t2-t1);
 
   fclose(fp);
-return 0;
+
+  return 0;
 } 
+//  for(int i=0; i<array_size; i++)
+//  {
+//    for(int j=0; j<array_size; j++)
+//    {
+//      matrix[i* array_size + j]=0; // initializing all values of the array to be 0 (no relation/edges between nodes)
+//    }
+//  } 
