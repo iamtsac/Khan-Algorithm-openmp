58ae6355a0a2264b00b594fac5a951022b351402
iamtsac <iamtsac@gmail.com>
2020-06-08 14:24:55 +0300

    IDs now start from 0

--- graphs.c
+++ graphs.c
@@ -3,44 +3,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define MIN_PER_RANK	2	/* Nodes/Rank: How 'fat' the DAG should be.  */
-#define MAX_PER_RANK	7
-#define PERCENT		10	/* Chance of having an Edge.  */
+#define MIN_PER_RANK  2 /* Nodes/Rank: How 'fat' the DAG should be.  */
+#define MAX_PER_RANK  7
+#define PERCENT   10  /* Chance of having an Edge.  */
 
 int main(int argc, char *argv[])
 {
-	int	i, j, k, nodes = 0, edges = 0;
-	int	ranks = atoi(argv[1]);
-	char	command[256];
-	FILE	*outfile;
+  int i, j, k, nodes = 0, edges = 0;
+  int ranks = atoi(argv[1]);
+  char  command[256];
+  FILE  *outfile;
 
-	outfile = fopen("dag.txt", "w");
+  srand(time(NULL));
 
-	srand(time(NULL));
+  outfile = fopen("dag.txt", "w");
 
+  for (i = 0; i < ranks; i++) {
+    /* New nodes of 'higher' rank than all nodes generated till now.  */
+    int new_nodes = MIN_PER_RANK + (rand () % (MAX_PER_RANK - MIN_PER_RANK + 1));
 
-	for (i = 0; i < ranks; i++) {
-		/* New nodes of 'higher' rank than all nodes generated till now.  */
-		int new_nodes = MIN_PER_RANK + (rand () % (MAX_PER_RANK - MIN_PER_RANK + 1));
+    /* Edges from old nodes ('nodes') to new ones ('new_nodes').  */
+    for (j = 0; j < nodes; j++) {
+      for (k = 0; k < new_nodes; k++) {
+        if ((rand() % 100) < PERCENT) {
+          fprintf(outfile, "%d %d\n", j, k + nodes);
+          edges++;
+        }
+      }
+    }
 
-		/* Edges from old nodes ('nodes') to new ones ('new_nodes').  */
-		for (j = 1; j < (nodes+1); j++) {
-			for (k = 1; k < ( new_nodes+1 ); k++) {
-				if ((rand() % 100) < PERCENT) {
-					fprintf(outfile, "%d %d\n", j, k + nodes);
-					edges++;
-				}
-			}
-		}
+    nodes += new_nodes; /* Accumulate into old node set.  */
+  }
 
-		nodes += new_nodes; /* Accumulate into old node set.  */
-	}
-
-	fclose(outfile);
+  fclose(outfile);
 
   sprintf(command, "sed -i '1s/^/%d %d %d\\n/' dag.txt", nodes, nodes, edges);
 
-	system(command);
+  system(command);
 
-	return 0;
-}
+  return 0;
+} 
--- parallel.c
+++ parallel.c
@@ -5,8 +5,6 @@
 #include <stdbool.h>
 #include <math.h>
 
-int array_size, total_edges;
-
 struct node_info // Δημιουργουμε το struct που περιεχει τους κομβους και πληροφορίες για αυτους.
 {
   int id;
@@ -18,10 +16,10 @@ double get_time(void) // Ο high resolution timer.
   struct timeval tv;
   gettimeofday(&tv, NULL);
   return tv.tv_sec + 1e-6 * tv.tv_usec;
-}
+} 
 
-int* Kahn_Algorithm(int threads,int *L,  struct node_info nodes[array_size], bool *matrix); // Δηλωση του Αλγοριθμου του Kahn.
-
+unsigned int array_size, total_edges;
+int* Kahn_Algorithm(int threads,int *L,  struct node_info *nodes, bool *matrix); // Δηλωση του Αλγοριθμου του Kahn.
 
 //                   ███    ███  █████  ██ ███    ██
 //                   ████  ████ ██   ██ ██ ████   ██
@@ -32,7 +30,7 @@ int* Kahn_Algorithm(int threads,int *L,  struct node_info nodes[array_size], boo
 
 int main(int argc, char **argv)
 {
-  static char data[50];
+  static char data[5000];
   double t1,t2;
   FILE *fp = fopen("dag.txt", "r");
 
@@ -55,31 +53,32 @@ int main(int argc, char **argv)
   
   for(int i=0; i<array_size; i++) // Κανουμε initialize το struct και καθορίζουμε τα id.
   {
-    nodes[i].id = i+1;
+    nodes[i].id = i;
     nodes[i].in_edges = 0;
   }
 
   // Αρχικοποιουμε δυναμικα τους παρακατω πινακες για να μπορουμε να δουλεψουμε σε πολυ μεγαλα γραφηματα.
-  bool *matrix = (bool *)malloc(array_size *array_size * sizeof(bool));
+  bool *matrix = (bool *)malloc( pow(array_size,2) * sizeof(bool));
   int *L = (int *)malloc(array_size * sizeof(int));
   int *temp1 = (int *)malloc(total_edges * sizeof(int));
   int *temp2 = (int *)malloc(total_edges * sizeof(int));
 
-  // Αρχικοποιουμε το μητρωο γειτνιασης και μετραμε τις εισερχομενες ακμες
-  // Καθε κομβου διαβαζοντας τις πληροφορίες απο το αρχειο.  
-  for(int i=0; i<total_edges; i++)
+  for(int i=0; i<total_edges; i++) // Διαβαζουμε τις πληροφοριες απο το αρχειο για καθε κομβο.  
   {
     fgets(data, sizeof(data), fp);
-    fscanf(fp, "%d %d", &temp1[i],&temp2[i]);
-    matrix[(temp1[i] - 1 )* array_size + ( temp2[i] - 1 )] = 1;
-    nodes[temp2[i] - 1 ].in_edges++;
+    fscanf(fp, "%d %d", &temp1[i],&temp2[i]); 
+    // Αρχικοποιουμε το μητρωο γειτνιασης και μετραμε τις εισερχομενες ακμες
+    matrix[temp1[i] * array_size + temp2[i]] = true;
+    nodes[temp2[i]].in_edges++;
   }
 
+  free(temp1),free(temp2);
 
   t1=get_time();
   L = Kahn_Algorithm(atoi(argv[1]),L, nodes, matrix);
-  t2=get_time();
+  t2=get_time(); 
 
+  free(matrix);
 
   printf("Successful!\nThe Topological sort is: \n");
   for(int i=0; i<array_size; i++)
@@ -88,38 +87,37 @@ int main(int argc, char **argv)
     printf("%d ",L[i]);
   } 
 
+  printf("%d the edges\n",total_edges);
+  free(L);
+
   printf("\n\nThe algorithm took %lf seconds to complete.\n", t2-t1);
 
   fclose(fp);
 
   return 0;
-}
-
-
+} 
 //                   ██   ██  █████  ██   ██ ███    ██
 //                   ██  ██  ██   ██ ██   ██ ████   ██
 //                   █████   ███████ ███████ ██ ██  ██
 //                   ██  ██  ██   ██ ██   ██ ██  ██ ██
 //                   ██   ██ ██   ██ ██   ██ ██   ████
 
-
-int* Kahn_Algorithm(int threads,int *L,  struct node_info nodes[array_size], bool *matrix)
-{
+int* Kahn_Algorithm(int threads,int *L,  struct node_info *nodes, bool *matrix)
+{ 
   int *S=(int *)malloc(array_size * (sizeof(int)));
-  int counter=0,i=0;
-
-  #pragma omp parallel num_threads(threads) shared(i,counter,array_size,matrix,L,S)
+  unsigned int counter=0,i=0;
+  #pragma omp parallel  num_threads(threads) shared(total_edges,i,counter,array_size,matrix,L)
   { 
-    #pragma omp for reduction(+:S[:array_size])
+
+    #pragma omp for ordered reduction(+:S[:array_size])
     for(int i=0; i<array_size; i++)
     {
       if(nodes[i].in_edges == 0)
-      {
-        #pragma omp critical
-        {
-          S[counter]=nodes[i].id;
-          counter++; // Μετραει τα στοιχεια του S.
-        }
+      { 
+        #pragma omp ordered
+        S[counter]=nodes[i].id;
+        #pragma omp atomic
+        counter++; // Μετραει τα στοιχεια του S.  
       }
     }
 
@@ -131,13 +129,16 @@ int* Kahn_Algorithm(int threads,int *L,  struct node_info nodes[array_size], boo
         L[i]=n;
         counter--,i++; // Ο counter μετραει τα στοιχεια στην S και το i την θεση που πρεπει να αποθηκευτουν τα στοιχεια στην L.
 
-        #pragma omp taskloop
+        #pragma omp taskloop reduction(-:total_edges)
         for (int j=0; j<array_size; j++)
         {
-          if( matrix[ (n-1) * array_size + j] == 1 )
+          if(matrix[n * array_size + j] == true)
           {
-            matrix[ (n-1) * array_size + j] = false; // Στο μητρωο σβηνουμε τις εξερχομενες ακμες του στοιχειου που πηραμε απο τον πινακα S.
+            matrix[n * array_size + j] = false; // Στο μητρωο σβηνουμε τις εξερχομενες ακμες του στοιχειου που πηραμε απο τον πινακα S.
             nodes[j].in_edges--; // Και ενημερωνουμε το struct για τον αριθμο των εισερχομενων ακμων.
+            total_edges--;
+            //printf("%d the edges\n",total_edges);
+
             if(nodes[j].in_edges == 0) // Αν καποιος κομβος δεν εχει εισερχομενες ακμες τον βαζουμε στον S.
             {
               #pragma omp critical
@@ -152,15 +153,15 @@ int* Kahn_Algorithm(int threads,int *L,  struct node_info nodes[array_size], boo
     }
   }
 
-
-  if( L[array_size-1]==0 || L[array_size]!=0 ) // Για να μην υπαρχει κυκλος πρεπει το μεγεθος του πινακα L να είναι ισο με το array_size.
+  if(total_edges != 0) // Για να μην υπαρχει κυκλος πρεπει να μην υπαραχουν αλλες ακμες.
   {
-    perror("The graph contains one or more cycles!\n");
+    printf("The graph contains one or more cycles!\n");
     exit(EXIT_FAILURE);
   }
 
   else
   {
+    free(S);
     return L;
   }
 }
